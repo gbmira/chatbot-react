1a59551ba337de29f194f7ad6b06ff667cc35c33
Logon Aluno <logonrmlocal@fiap.com.br>
2023-09-22 11:46:37 -0300

    finalizado chatbot

--- src/App.js
+++ src/App.js
@@ -1,24 +1,121 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import ChatBot from 'react-simple-chatbot';
+import { Segment, Button } from 'semantic-ui-react';
+import { ThemeProvider } from 'styled-components';
+
+const theme = {
+  background: '#fff',
+  fontFamily: 'coolvetica',
+  headerBgColor: '#2171AC',
+  headerFontColor: '#fff',
+  headerFontSize: '15px',
+  botBubbleColor: '#2171AC',
+  botFontColor: '#fff',
+  userBubbleColor: '#4b4b4b',
+  userFontColor: 'white',
+};
+
+const steps = [
+  {
+    id: '1',
+    message: 'Olá, como posso te ajudar?',
+    trigger: '2'
+  },
+  {
+    id: '2',
+    options: [
+      { value: 1, label: 'Como funciona os serviços dos produtos?', trigger: 'servico' },
+      { value: 2, label: 'Planos', trigger: 'plano' },
+      { value: 3, label: 'Qual o canal de contato?', trigger: 'contato' },
+      { value: 4, label: 'Outros..', trigger: 'outros' },
+    ],
+  },
+  {
+    id: 'servico',
+    message: 'Você poderá encontrar informações sobre nossos serviços na aba sobre e na tela home.',
+    trigger: 'ultima'
+  },
+  {
+    id: 'plano',
+    message: 'Temos 3 opções de planos com preços acessíveis! Prata, Ouro e Diamante (Você poderá encontrar mais informações na aba de planos).',
+    trigger: 'ultima'
+  },
+  {
+    id: 'contato',
+    message: 'Você conseguirá entrar em contato conosco preenchendo nosso formulário na página de contato.',
+    trigger: 'ultima'
+  },
+  {
+    id: 'outros',
+    options: [
+      { value: 1, label: 'Como faço para utilizar?', trigger: 'utilizar' },
+      { value: 2, label: 'Quais tecnologias vocês utilizam?', trigger: 'tecnologia' },
+      { value: 3, label: 'Qualquer outra pergunta...', trigger: 'outraPergunta' },
+    ],
+  },
+  {
+    id: 'utilizar',
+    message: 'Crie uma conta e faça login para desfrutar do seu potencial deepleads.',
+    trigger: 'ultima'
+  },
+  {
+    id: 'tecnologia',
+    message: 'Utilizamos diversas tecnologias como: inteligencia artificial, mineração de dados e disparo de mensagens via whatsapp.',
+    trigger: 'ultima'
+  },
+  {
+    id: 'outraPergunta',
+    message: 'Para mais informações, entre em contato através de dos nossos canais de atendimento, por favor.',
+    trigger: 'ultima'
+  },
+  {
+    id: 'ultima',
+    message: 'Tem mais alguma outra dúvida?',
+    trigger: 'opcoes'
+  },
+  {
+    id: 'opcoes',
+    options: [
+      { value: 1, label: 'Sim', trigger: 'sim' },
+      { value: 2, label: 'Não', trigger: 'nao' },
+    ],
+  },
+  {
+    id: 'nao',
+    message: 'Ótimo! Fico feliz em sanar suas dúvidas. Até mais, Obrigado!',
+    end: true
+  },
+  {
+    id: 'sim',
+    message: 'Abaixo irei disponibilizar novamente as opções:',
+    trigger: '2'
+  },
+]
 
 function App() {
+  const [chatbotOpen, setChatbotOpen] = useState(false);
+
+  const toggleChatbot = () => {
+    setChatbotOpen(!chatbotOpen);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      {chatbotOpen && (
+        <Segment floated='left'>
+          <ThemeProvider theme={theme}>
+            <ChatBot steps={steps} />
+          </ThemeProvider>
+        </Segment>
+      )}
+      <Button
+        floated='left'
+        onClick={toggleChatbot}
+        color={chatbotOpen ? 'red' : 'green'}
+      >
+        {chatbotOpen ? 'Fechar Chatbot' : 'Abrir Chatbot'}
+      </Button>
+    </>
   );
 }
 
